7d50ae666daa49155c319ac5cf4479849adaa7d8
Yeseul Kim <dptmfl3018@naver.com>
2025-09-11 13:33:31 +0900

    UserSession 토큰 관련 수정

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -2,96 +2,17 @@ import { UserSession } from "../../services/UserSession.js";
 
 const BASE_URL = "https://api.wenivops.co.kr/services/open-market";
 
-const menuItems = document.querySelectorAll(".menu-item");
-const loginForm = document.getElementById("login-form");
-const loginBtn = document.querySelector(".login-btn");
-const loginErr = document.getElementById("login-error");
-const idInput = document.getElementById("user-id");
-const pwInput = document.getElementById("password");
+const $menuItems = document.querySelectorAll(".menu-item");
+const $loginForm = document.getElementById("login-form");
+const $loginBtn = document.querySelector(".login-btn");
+const $loginErr = document.getElementById("login-error");
+const $idInput = document.getElementById("user-id");
+const $pwInput = document.getElementById("password");
 let currentRole = "BUYER"; // 기본값 -> 구매자
 
 const session = new UserSession();
 
-// [ Event ] ================
-// ===== 회원 메뉴 선택 =====
-menuItems.forEach((menuItem) => {
-  menuItem.addEventListener("click", (e) => {
-    const btn = menuItem.querySelector("button[data-role");
-    if (!btn) return;
-
-    currentRole = btn.dataset.role;
-    console.log("선택된 회원의 역할: ", currentRole);
-
-    menuItems.forEach((item) => item.classList.remove("on")); // 일단 현재 on 클래스 전체 삭제
-    menuItem.classList.add("on");
-  });
-});
-
-// ===== 폼 제출 -> 로그인 정보 제출 =====
-loginForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const username = idInput.value.trim();
-  const password = pwInput.value;
-
-  // 유효성 검사
-  if (!username) {
-    showMessage("아이디를 입력해 주세요.");
-    idInput.focus();
-    return;
-  }
-  if (!password) {
-    showMessage("비밀번호를 입력해 주세요.");
-    pwInput.focus();
-    return;
-  }
-
-  // 중복 제출 방지
-  loginBtn?.setAttribute("disabled", "");
-
-  try {
-    const { access, refresh, user } = await login(username, password);
-
-    const serverRole = String(user?.user_type || "");
-    if (!serverRole || serverRole !== currentRole) {
-      const roleAccessMsg =
-        currentRole === "BUYER"
-          ? "판매회원으로 로그인해주세요."
-          : "구매회원으로 로그인해주세요.";
-      alert(roleAccessMsg);
-      idInput.focus();
-      return;
-    }
-
-    // UserSession 클래스 활용
-    session.save({ access, refresh, user });
-
-    alert("로그인 성공!");
-
-    // 이전 화면으로 돌아가기 (SessionStorage)
-    const redirect =
-      sessionStorage.getItem("redirect_after_login") || "../index.html";
-    sessionStorage.removeItem("redirect_after_login"); // 로그인 성공 후 제거
-
-    if (redirect) {
-      location.replace(redirect);
-    } else if (
-      document.referrer &&
-      new URL(document.referrer).origin === location.origin
-    ) {
-      history.back();
-    } else {
-      location.replace("../index.html");
-    }
-  } catch (err) {
-    console.error(err);
-    showMessage(err.message || "로그인 실패");
-  } finally {
-    loginBtn?.removeAttribute("disabled");
-  }
-});
-
-// ===== API 서버 연결 =====
+// ----- API 서버 연결 -----
 async function login(username, password) {
   const res = await fetch(`${BASE_URL}/accounts/login/`, {
     method: "POST",
@@ -102,13 +23,84 @@ async function login(username, password) {
   const data = await res.json();
 
   if (!res.ok) {
-    throw new Error(data.error || data.detail || "로그인 실패");
+    if (res.status === 401) {
+      throw new Error("아이디 또는 비밀번호가 올바르지 않습니다.");
+    } else {
+      throw new Error(data.error || data.detail || "로그인 실패");
+    }
   }
 
   return data;
 }
 
-// ===== 에러 메시지 표시 =====
+// ----- 에러 메시지 표시 -----
 function showMessage(message) {
-  loginErr.textContent = message;
+  $loginErr.textContent = message;
 }
+
+// Event
+// ----- 회원 메뉴 선택 -----
+$menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", (e) => {
+    const btn = menuItem.querySelector("button[data-role]");
+    if (!btn) return;
+
+    currentRole = btn.dataset.role;
+    console.log("선택된 회원의 역할: ", currentRole);
+
+    $menuItems.forEach((item) => item.classList.remove("on")); // 일단 현재 on 클래스 전체 삭제
+    menuItem.classList.add("on");
+  });
+});
+
+// ----- 로그인 폼 제출 처리  -----
+$loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  showMessage("");
+
+  const username = $idInput.value.trim();
+  const password = $pwInput.value;
+
+  // 유효성 검사
+  if (!username) {
+    showMessage("아이디를 입력해 주세요.");
+    $idInput.focus();
+    return;
+  }
+  if (!password) {
+    showMessage("비밀번호를 입력해 주세요.");
+    $pwInput.focus();
+    return;
+  }
+
+  // 중복 제출 방지
+  $loginBtn?.setAttribute("disabled", "");
+
+  try {
+    const { access, refresh, user } = await login(username, password);
+
+    const userRole = String(user?.user_type || "").toUpperCase();
+    if (!userRole || userRole !== currentRole) {
+      alert("선택한 회원 유형으로 로그인할 수 없습니다.");
+      $idInput.focus();
+      return;
+    }
+
+    // 로그인 성공시 토큰 저장
+    session.save({ access, refresh, user });
+
+    alert("로그인 성공!");
+
+    // 이전 화면으로 돌아가기
+    if (document.referrer && !document.referrer.includes("/login")) {
+      history.back();
+    } else {
+      location.replace("../../public/index.html");
+    }
+  } catch (err) {
+    console.error(err);
+    showMessage(err.message || "로그인 실패했습니다. 다시 시도해 주세요.");
+  } finally {
+    $loginBtn?.removeAttribute("disabled");
+  }
+});
--- src/services/UserSession.js
+++ src/services/UserSession.js
@@ -1,6 +1,10 @@
 export class UserSession {
-  constructor(storage = localStorage) {
+  constructor(
+    storage = localStorage,
+    baseUrl = "https://api.wenivops.co.kr/services/open-market"
+  ) {
     this.storage = storage;
+    this.baseUrl = baseUrl;
   }
 
   // 사용자 정보 조회
@@ -15,7 +19,7 @@ export class UserSession {
   // 사용자 역할 조회
   getRole() {
     const user = this.getUser();
-    return this.user?.user_type?.toUpperCase() || null;
+    return user?.user_type?.toUpperCase() || null;
   }
 
   // 구매자 / 판매자 구분 -> ture, false 반환
@@ -23,7 +27,7 @@ export class UserSession {
     return this.getRole() === "BUYER";
   }
   isSeller() {
-    return this.getRole === "SELLER";
+    return this.getRole() === "SELLER";
   }
 
   // Access Token 조회
@@ -31,16 +35,66 @@ export class UserSession {
     return this.storage.getItem("omkt_access");
   }
 
-  //
+  // Refresh Token 조회
+  getRefresh() {
+    return this.storage.getItem("omkt_refresh");
+  }
+
+  // 로그인 상태 확인 (access, refresh token check)
   isAuthed() {
     const access = this.getAccess();
-    const exp = Number(this.storage.getItem("omkt_aexp") || 0);
-    if (!access) return false; // 토큰 없으면 false 반환 -> 로그인 안됨
-    if (exp || Date.now() >= exp) {
-      // 만료 시간이거나 이미 지났다면
-      return false; // 로그인 만료
+    const rexp = Number(this.storage.getItem("omkt_rexp") || 0);
+    if (!access) return false;
+    if (!rexp || Date.now() >= rexp) {
+      this.clear(); // 로그아웃
+      return false;
     }
-    return true; // 나머지는 로그인 유지
+    return true;
+  }
+
+  // 토큰 재발급 -> 성공/ 실패 여부를 간단히 알 수 있는 로직 필요
+  async refreshAccessToken() {
+    const refreshToken = this.getRefresh();
+    const rexp = Number(this.storage.getItem("omkt_rexp") || 0);
+
+    // refresh 토큰이 없거나 만료됐을 때
+    if (!refreshToken || Date.now() >= rexp) {
+      this.clear();
+      // 로그인창으로 보내버리기
+      alert("다시 로그인해 주세요.");
+      location.replace("../login/login.html");
+    }
+
+    try {
+      const res = await fetch(`${this.baseUrl}/accounts/token/refresh/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          refresh: refreshToken,
+        }),
+      });
+
+      if (!res.ok) {
+        throw new Error("토큰 재발급 실패");
+      }
+
+      const data = await res.json();
+      const newAccessToken = data.access;
+
+      const now = Date.now();
+      this.storage.setItem("omkt_access", newAccessToken);
+      this.storage.setItem("omkt_aexp", String(now + 5 * 60 * 1000));
+
+      return newAccessToken;
+    } catch (err) {
+      // 서버 오류
+      this.clear();
+      // 에러 났을 때 어떤 처리가 필요할지
+      alert("다시 로그인해 주세요.");
+      location.replace("../login/login.html");
+    }
   }
 
   // 세션 정보 저장 (로그인)
@@ -49,7 +103,7 @@ export class UserSession {
     this.storage.setItem("omkt_access", access);
     this.storage.setItem("omkt_refresh", refresh);
     this.storage.setItem("omkt_user", JSON.stringify(user));
-    this.storage.setItem("omkt_aexp", String(now + 5 * 60 * 1000));
+    this.storage.setItem("omkt_aexp", String(now + 5 * 60 * 1000)); // 업데이트 필요 -> 비동기
     this.storage.setItem("omkt_rexp", String(now + 24 * 60 * 60 * 1000));
   }
 
