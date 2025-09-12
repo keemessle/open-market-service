9a1f0cb0a60111dbb762ad36e1bc817d97c491d6
김민주 <891km@ewhain.net>
2025-09-12 14:37:17 +0900

    액션 리스트 아이콘 svg로 변경

--- src/components/header.js
+++ src/components/header.js
@@ -3,24 +3,37 @@ import { UserSession } from "../services/UserSession.js";
 //로그인별 actions-list
 const actionCart = {
   href: "./cart.html",
-  img: "./assets/images/icons/icon-shopping-cart.svg",
-  alt: "장바구니 바로가기",
+  svgTag: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+  <path d="M12.889 28.4444C13.6254 28.4444 14.2223 27.8475 14.2223 27.1111C14.2223 26.3747 13.6254 25.7778 12.889 25.7778C12.1526 25.7778 11.5557 26.3747 11.5557 27.1111C11.5557 27.8475 12.1526 28.4444 12.889 28.4444Z" fill="black" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M25.3333 28.4444C26.0697 28.4444 26.6667 27.8475 26.6667 27.1111C26.6667 26.3747 26.0697 25.7778 25.3333 25.7778C24.597 25.7778 24 26.3747 24 27.1111C24 27.8475 24.597 28.4444 25.3333 28.4444Z" fill="black" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M2.66699 2.66669H7.51548L10.764 18.5359C10.8748 19.0816 11.1784 19.5717 11.6216 19.9205C12.0648 20.2694 12.6194 20.4547 13.1882 20.444H24.97C25.5389 20.4547 26.0934 20.2694 26.5366 19.9205C26.9798 19.5717 27.2834 19.0816 27.3943 18.5359L29.3337 8.59247H8.7276" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+  `,
+  label: "장바구니 바로가기",
   text: "장바구니",
   id: "action-cart",
 };
 
 const actionLogin = {
   href: "./login.html",
-  img: "./assets/images/icons/icon-user.svg",
-  alt: "로그인 바로가기",
+  svgTag: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+  <path d="M26.6663 28V25.3333C26.6663 23.9188 26.1044 22.5623 25.1042 21.5621C24.104 20.5619 22.7475 20 21.333 20H10.6663C9.25185 20 7.8953 20.5619 6.8951 21.5621C5.89491 22.5623 5.33301 23.9188 5.33301 25.3333V28" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M16.0003 14.6667C18.9458 14.6667 21.3337 12.2789 21.3337 9.33333C21.3337 6.38781 18.9458 4 16.0003 4C13.0548 4 10.667 6.38781 10.667 9.33333C10.667 12.2789 13.0548 14.6667 16.0003 14.6667Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+  `,
+  label: "로그인 바로가기",
   text: "로그인",
   id: "action-login",
 };
 
 const actionMypage = {
   href: "#",
-  img: "./assets/images/icons/icon-user.svg",
-  alt: "마이페이지 바로가기",
+  svgTag: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+  <path d="M26.6663 28V25.3333C26.6663 23.9188 26.1044 22.5623 25.1042 21.5621C24.104 20.5619 22.7475 20 21.333 20H10.6663C9.25185 20 7.8953 20.5619 6.8951 21.5621C5.89491 22.5623 5.33301 23.9188 5.33301 25.3333V28" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M16.0003 14.6667C18.9458 14.6667 21.3337 12.2789 21.3337 9.33333C21.3337 6.38781 18.9458 4 16.0003 4C13.0548 4 10.667 6.38781 10.667 9.33333C10.667 12.2789 13.0548 14.6667 16.0003 14.6667Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+  `,
+  label: "마이페이지 바로가기",
   text: "마이페이지",
   id: "action-mypage",
   dropdown: `
@@ -33,8 +46,12 @@ const actionMypage = {
 
 const actionSeller = {
   href: "./seller-center.html",
-  img: "./assets/images/icons/icon-shopping-bag.svg",
-  alt: "판매자 센터 바로가기",
+  svgTag: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+  <path d="M8 2.66669L4 8.00002V26.6667C4 27.3739 4.28095 28.0522 4.78105 28.5523C5.28115 29.0524 5.95942 29.3334 6.66667 29.3334H25.3333C26.0406 29.3334 26.7189 29.0524 27.219 28.5523C27.719 28.0522 28 27.3739 28 26.6667V8.00002L24 2.66669H8Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M4 8H28" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  <path d="M21.3337 13.3333C21.3337 14.7478 20.7718 16.1044 19.7716 17.1045C18.7714 18.1047 17.4148 18.6666 16.0003 18.6666C14.5858 18.6666 13.2293 18.1047 12.2291 17.1045C11.2289 16.1044 10.667 14.7478 10.667 13.3333" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`,
+  label: "판매자 센터 바로가기",
   text: "판매자 센터",
   id: "action-seller",
 };
@@ -48,9 +65,9 @@ export function createHeader() {
   const loginSession = new UserSession(localStorage);
   const isLoggedIn = loginSession.isAuthed();
   // const isBuyer = loginSession.isBuyer();
-  const isBuyer = false;
+  const isBuyer = true;
   // const isSeller = loginSession.isSeller();
-  const isSeller = true;
+  const isSeller = false;
 
   // header 없으면 생성
   let existHeader = document.querySelector("header");
@@ -98,25 +115,24 @@ export function createHeader() {
 
     actionsDataList.forEach((action) => {
       const li = document.createElement("li");
+      li.classList.add("actions-item");
 
       // 마이페이지 버튼 li > 드롭다운
       if (action.dropdown) {
         li.innerHTML = action.dropdown;
-        li.className = "dropdown-wrap";
+        li.classList.add("dropdown-parent");
       }
 
       const a = document.createElement("a");
       a.href = action.href;
       a.id = action.id;
+      a.innerHTML = action.svgTag;
+      a.ariaLabel = action.label;
 
-      const img = document.createElement("img");
-      img.src = action.img;
-      img.alt = action.alt;
+      const span = document.createElement("span");
+      span.textContent = action.text;
 
-      const p = document.createElement("p");
-      p.textContent = action.text;
-
-      a.append(img, p);
+      a.append(span);
       li.appendChild(a);
       $actionsList.appendChild(li);
     });
@@ -130,8 +146,6 @@ export function createHeader() {
     const $mypageDropdown = document.querySelector(".dropdown-mypage");
     if ($mypageBtn) {
       $mypageBtn.addEventListener("click", () => {
-        $mypageBtn.querySelector("img").src =
-          "./assets/images/icons/icon-user-2.svg";
         $mypageDropdown.classList.add("active");
       });
     }
@@ -144,11 +158,17 @@ export function createHeader() {
       )
         return;
 
-      $mypageBtn.querySelector("img").src =
-        "./assets/images/icons/icon-user.svg";
       $mypageDropdown.classList.remove("active");
+      $mypageDropdown.closest(".actions-item").classList.remove("active");
     });
   }
+
+  const $actionsList = document.querySelector(".actions-list");
+  $actionsList.addEventListener("click", (e) => {
+    const target = e.target.closest(".actions-item");
+    if (!target) return;
+    target.classList.add("active");
+  });
 
   // 로그아웃 버튼 이벤트
   const $logoutBtn = document.getElementById("btn-logout");
