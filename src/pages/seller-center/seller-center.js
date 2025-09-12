173aafd23a27cf7734e01ce1bbbde6e2f16bbba7
김민주 <891km@ewhain.net>
2025-09-12 17:22:44 +0900

    HTML 기본 구조

---
+++ src/pages/seller-center/seller-center.js
@@ -0,0 +1,7 @@
+import { createHeader } from "../../components/header.js";
+import { createFooter } from "../../components/footer.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  createHeader();
+  createFooter();
+});
